faf6e5e3e74e7d2063db8cb945500a8b4d36a875
fontesrafa <rafaelfontes777@hotmail.com>
2022-05-04 09:09:46 -0300

    Started Xylophone App

---
+++ xylophone_flutter/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Xylophone());
+}
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
